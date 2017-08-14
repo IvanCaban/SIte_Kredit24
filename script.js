8d950a63d5d8178999651bdf38025ef26370d16f
IvanCaban <ivanheinlein@gmail.com>
2017-08-14 22:01:54 +0300

    14.08 input_range + js

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 }
+/******************************************************************************/
+
+var selectedMoney = document.getElementById("selected-money");
+var rangeMoney = document.getElementById("range-money");
+
+rangeMoney.oninput = function() {
+	var tempStr = rangeMoney.value.toString();
+	selectedMoney.innerHTML = tempStr[0] + " " +tempStr[1]+tempStr[2]+tempStr[3] + " " +tempStr[4]+tempStr[5]+tempStr[6];
+}
+
+var selectedDays = document.getElementById("selected-days");
+var rangeDays = document.getElementById("range-days");
+
+rangeDays.oninput = function(){
+	selectedDays.innerHTML = rangeDays.value;
+}
