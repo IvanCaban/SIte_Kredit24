480ecd89074af1e66bf58903ce548b7b4618959c
IvanCaban <ivanheinlein@gmail.com>
2017-08-17 19:44:39 +0300

    17.08 range works

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-var modal = document.getElementById("modal");
+var modalForm = document.getElementById("modal-form");
 var logBtn = document.getElementById("log-btn");
-var close = document.getElementById("modal-close");
+var closeForm = document.getElementById("modal-form-close");
 
 logBtn.onclick = function(){
-	modal.style.display = "block";
+	modalForm.style.display = "block";
 }
 
-close.onclick = function() {
-	modal.style.display = "none";
+closeForm.onclick = function() {
+	modalForm.style.display = "none";
 }
 
 window.onclick = function(event) {
-	if(event.target == modal){
-		modal.style.display = "none";
+	if(event.target == modalForm){
+		modalForm.style.display = "none";
 	}
 }
 /******************************************************************************/
+var modalSendSms = document.getElementById("modal-send-sms");
+var getMoneyBtn = document.getElementById("get-money-btn");
+var closeSendSms = document.getElementById("modal-send-sms-close");
 
+getMoneyBtn.onclick = function(){
+	modalSendSms.style.display = "block";
+}
+
+closeSendSms.onclick = function(){
+	modalSendSms.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if(event.target == modalSendSms){
+		modalSendSms.style.display = "none";
+	}
+}
+/******************************************************************************/
 var selectedMoney = document.getElementById("selected-money");
 var rangeMoney = document.getElementById("range-money");
+var lineMoney = document.getElementById("line-money");
+var distansMoney = rangeMoney.max - rangeMoney.min;
 
 rangeMoney.oninput = function() {
 	var tempStr = rangeMoney.value.toString();
 	selectedMoney.innerHTML = tempStr[0] + " " +tempStr[1]+tempStr[2]+tempStr[3] + " " +tempStr[4]+tempStr[5]+tempStr[6];
+	lineMoney.style.width = ((rangeMoney.value -rangeMoney.min) * 100 / distansMoney) + "%";
 }
 
 var selectedDays = document.getElementById("selected-days");
 var rangeDays = document.getElementById("range-days");
+var lineDays = document.getElementById("line-days");
+var distansDays = rangeDays.max - rangeDays.min;
 
 rangeDays.oninput = function(){
 	selectedDays.innerHTML = rangeDays.value;
+	lineDays.style.width = ((rangeDays.value -rangeDays.min) * 100 / distansDays) + "%";
 }
